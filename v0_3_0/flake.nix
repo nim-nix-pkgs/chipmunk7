2f0d9fcd3f0007f7e62431b6607950819fbdd51d
hugosenari <hugosenari@gmail.com>
2022-03-22 00:25:41 -0300

    chore: re-index ../nimpkgs/c/chipmunk7/v0_3_0

--- v0_3_0/flake.nix
+++ v0_3_0/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-chipmunk7-v0_3_0.flake = false;
   inputs.src-chipmunk7-v0_3_0.owner = "avahe-kellenberger";
-  inputs.src-chipmunk7-v0_3_0.ref   = "refs/tags/v0.3.0";
+  inputs.src-chipmunk7-v0_3_0.ref   = "v0_3_0";
   inputs.src-chipmunk7-v0_3_0.repo  = "nim-chipmunk";
   inputs.src-chipmunk7-v0_3_0.type  = "github";
   
